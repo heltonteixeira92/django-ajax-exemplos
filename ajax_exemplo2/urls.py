0c89dd6a3ecdde01917990e078328f4cd3d06396
Helton Teixeira de Souza <helton.souza@ongame.corp.com.br>
2022-09-02 14:07:38 -0300

    form post

--- ajax_exemplo2/urls.py
+++ ajax_exemplo2/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('getProfiles', views.getProfiles, name='getProfiles')
+    path('getProfiles', views.getProfiles, name='getProfiles'),
+    path('create', views.create, name='create'),
+    path('formulario', views.profile_form, name='profile_form'),
 ]
--- ajax_exemplo2/views.py
+++ ajax_exemplo2/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Profile
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 
 
 # Create your views here.
@@ -12,3 +12,23 @@ def getProfiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': list(profiles.values())}
     return JsonResponse(context)
+
+
+def profile_form(request):
+    return render(request, 'ajax_exemplo2/submit-form.html')
+
+
+def create(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST['email']
+        bio = request.POST['bio']
+
+        new_profile = Profile.objects.create(
+            name=name,
+            email=email,
+            bio=bio
+        )
+        new_profile.save()
+        return HttpResponse('New Profile Created Successfully')
+
